27f13803188c79c8b3b8b529af2b536727dc6de8
Anne-Julie Labrie <2509704@cmontmorency.qc.ca>
2026-04-14 13:57:23 -0400

    meow

--- assets/course.js
+++ assets/course.js
@@ -6,11 +6,11 @@ let voiture1Width;
 let voiture2Width;
 let compteARebours;
 
-
 function compteARebours() {
     /* 
         Affiche un compte à rebours, démarrant à 3 et se terminant à 0, pour le lancement de la course.
     */
+    window.alert("wowooowowowo");
 }
 
 function chronometre() {
